e874c5c49e81db09a204f0c541a1e00a19554c32
laqiiz <mano.junki@gmail.com>
2020-08-24 23:36:47 +0900

    fix #11 Add Unwrap func

--- reguerr_test.go
+++ reguerr_test.go
@@ -32,3 +32,36 @@ func TestCodeError_Error(t *testing.T) {
 		})
 	}
 }
+
+func TestReguError_Unwrap(t *testing.T) {
+
+	var (
+		inErr = errors.New("internal error")
+	)
+
+	tests := []struct {
+		name    string
+		in      *ReguError
+		wantErr error
+	}{
+		{
+			name: "exists_err",
+			in: &ReguError{
+				code:       "1003",
+				level:      Error,
+				statusCode: 500,
+				format:     "invalid input parameter: %v",
+				args:       []interface{}{`{"key":"hoge"}`},
+				err:        inErr,
+			},
+			wantErr: inErr,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := errors.Unwrap(tt.in); err != tt.wantErr {
+				t.Errorf("errors.Unwrap() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
