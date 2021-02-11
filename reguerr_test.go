0d96acfdba17199312f300902aa822626b33b195
Koki Honda <k.honda.kh@future.co.jp>
2021-02-11 10:25:28 +0900

    TestErrorの内容をTestCodeError_Errorに移した

--- reguerr_test.go
+++ reguerr_test.go
@@ -6,22 +6,63 @@ import (
 )
 
 func TestCodeError_Error(t *testing.T) {
+
+	var (
+		inErr = errors.New("internal error")
+	)
+
 	tests := []struct {
 		name string
 		in   ReguError
 		want string
 	}{
 		{
-			name: "",
+			name: "sucess path",
 			in: ReguError{
 				code:       "1003",
 				level:      Error,
 				statusCode: 500,
 				format:     "invalid input parameter: %v",
 				args:       []interface{}{`{"key":"hoge"}`},
-				err:        errors.New("internal error"),
+				err:        inErr,
 			},
-			want: `[1003]invalid input parameter: [{"key":"hoge"}]: internal error`,
+			want: `[1003] invalid input parameter: [{"key":"hoge"}]: internal error`,
+		},
+		{
+			name: "no placeholder",
+			in: ReguError{
+				code:       "1003",
+				level:      Error,
+				statusCode: 500,
+				format:     "Permission Denied",
+				args:       []interface{}{`{"key":"hoge"}`},
+				err:        inErr,
+			},
+			want: "[1003] Permission Denied: internal error",
+		},
+		{
+			name: "too many args",
+			in: ReguError{
+				code:       "1003",
+				level:      Error,
+				statusCode: 500,
+				format:     "other user updated: key=%s",
+				args:       []interface{}{"KEY", "KEY2"},
+				err:        inErr,
+			},
+			want: "[1003] other user updated: key=[KEY]: internal error",
+		},
+		{
+			name: "too few args",
+			in: ReguError{
+				code:       "1003",
+				level:      Error,
+				statusCode: 500,
+				format:     "other user updated: key=%s, %s",
+				args:       []interface{}{"KEY"},
+				err:        inErr,
+			},
+			want: "[1003] other user updated: key=[KEY]: internal error",
 		},
 	}
 	for _, tt := range tests {
@@ -61,73 +102,6 @@ func TestReguError_Unwrap(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := errors.Unwrap(tt.in); err != tt.wantErr {
 				t.Errorf("errors.Unwrap() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestError(t *testing.T) {
-	var (
-		inErr = errors.New("internal error")
-	)
-	tests := []struct {
-		name       string
-		in         *ReguError
-		wantString string
-	}{
-		{
-			name: "sucess path",
-			in: &ReguError{
-				code:       "1002",
-				level:      Error,
-				statusCode: 500,
-				format:     "other user updated: key=%s",
-				args:       []interface{}{"KEY"},
-				err:        inErr,
-			},
-			wantString: "[1002] other user updated: key=[KEY]: internal error",
-		},
-		{
-			name: "no placeholder",
-			in: &ReguError{
-				code:       "1003",
-				level:      Error,
-				statusCode: 500,
-				format:     "Permission Denied",
-				args:       []interface{}{},
-				err:        inErr,
-			},
-			wantString: "[1003] Permission Denied: internal error",
-		},
-		{
-			name: "too many args",
-			in: &ReguError{
-				code:       "1003",
-				level:      Error,
-				statusCode: 500,
-				format:     "other user updated: key=%s",
-				args:       []interface{}{"KEY", "KEY2"},
-				err:        inErr,
-			},
-			wantString: "[1003] other user updated: key=[KEY]: internal error",
-		},
-		{
-			name: "too few args",
-			in: &ReguError{
-				code:       "1003",
-				level:      Error,
-				statusCode: 500,
-				format:     "other user updated: key=%s, %s",
-				args:       []interface{}{"KEY"},
-				err:        inErr,
-			},
-			wantString: "[1003] other user updated: key=[KEY]: internal error",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.in.Error(); err != tt.wantString {
-				t.Errorf("errors.Unwrap() error = %v, wantString %v", err, tt.wantString)
 			}
 		})
 	}
